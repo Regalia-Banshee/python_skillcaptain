19014c505d01ed410c057b979846f6010ea488cb
Siddharth Nair <siddhurajannair@gmail.com>
2023-09-27 01:13:34 +0530

    second commit

---
+++ Python1 day3.py
@@ -0,0 +1,5 @@
+a,b =2,4
+print(f"The sum of a and b is:{a+b}")
+print(f"The difference of b from a is:{b-a}")
+print(f"The product of a and b is:{a*b}")
+print(f"The Quotient of a divided by b is:{a%b}")
