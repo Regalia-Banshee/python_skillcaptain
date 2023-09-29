bdf65848cdd611e56a89f07e5406eb2037fc1629
Siddharth Nair <siddhurajannair@gmail.com>
2023-09-29 13:50:43 +0530

    fifth commit

---
+++ Day 5.py
@@ -0,0 +1,13 @@
+a=input("Do you want to list numbers from 1-10?(y/n)")
+
+while a == "n":
+    print("Please say yes :(")
+    a=input("Do you want to list numbers from 1-10?(y/n)")
+
+print(list(range(1,11)))
+
+a=['Sreerag','Sireesh','Sreedhar','Siddhanth','Sreekumar']
+print(a)
+for i in a:
+    print(f"Hello {i},Warm Welcome")
+
