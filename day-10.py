22bc110eae86314e78b5e892e8298a4105ae3c7a
Siddharth Nair <siddhurajannair@gmail.com>
2023-10-01 18:54:42 +0530

    Final commit of P1

---
+++ day-10.py
@@ -0,0 +1,10 @@
+list1=[1,1,2,2,3,3,"Apple","Banana","Banana","Muskmelon",42,43,44,42]
+set1=set(list1)
+print(set1)
+print(type(set1))
+
+set2={1,5,4,76,99,123,"Muskmelon"}
+print(set2)
+list2=list(set2)
+print(list2)
+print(type(list2))
