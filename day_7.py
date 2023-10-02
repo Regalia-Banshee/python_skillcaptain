ccba073ec0f78e6db7aba5cd8aae9c6b958d877e
Siddharth Nair <siddhurajannair@gmail.com>
2023-10-02 22:45:06 +0530

    Updated day_7 code

--- day_7.py
+++ day_7.py
@@ -1,13 +1,20 @@
 #Defining a function to check if given numbers are odd or even , Since we know even numbers are always divisible by 2, 
 #remainder must be 0 if number is even
-def is_even(num):
+def  is_even(num):
     result=num%2
     return result
+number=1
+while number==True:
+    try:
+        num=int(input("Enter your number: "))
+        result=is_even(num)
+        if result == 0:
+            print("The number is even")
+        else:
+            print("The number is odd")
+        number = False
+    except ValueError:
+        print("Enter a number")
 
-num=int(input("Enter your number: "))
-result=is_even(num)
-if result==0:
-    print("Your number is even")
-else:
-    print("Your number is odd")
+
 
