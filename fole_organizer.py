e70d1c5f1f374eab2717aa15b04e3c3fd24c99d7
Regalia-Banshee <91406359+Regalia-Banshee@users.noreply.github.com>
2024-01-15 18:42:51 +0530

    Add files via upload

--- fole_organizer.py
+++ fole_organizer.py
@@ -1,16 +1,19 @@
 import os
 import shutil
 def file_organiser():
-    path=r'D:\unorganised files'
+    path=input("Enter Source path: ")
+    destination=input("Enter destination Path: ")
+
+
     list_of_file=os.listdir(path)
     for file in list_of_file:
         name,extension=os.path.splitext(file)
         extension=extension[1:]
-        if os.path.exists(path+'/'+extension):
-            shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
+        if os.path.exists(destination+'/'+extension):
+            shutil.move(path+'/'+file,destination+'/'+extension+'/'+file)
 
         else:
-            os.mkdir(path+'/'+extension)
-            shutil.move(path+'/'+file,path+'/'+extension+'/'+file)
+            os.mkdir(destination+'/'+extension)
+            shutil.move(path+'/'+file,destination+'/'+extension+'/'+file)
 
 file_organiser()
